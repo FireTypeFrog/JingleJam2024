5bd5718bf79606fdd7dc4f09d4e7f7e55bdb344a
FireTypeFrog <firetypefrog@proton.me>
2024-12-02 15:48:20 -0800

    player movement

--- entity/Player.cs
+++ entity/Player.cs
@@ -11,11 +11,16 @@ using Toybox.utils.math;
 namespace JingleJam2024.entity {
 	public class Player:Entity {
 
-		private const float TurnSpeed = 0.1f;
-		private const float MaxSpeed = 2;
+		private const float TurnSpeed = 0.05f;
+		private const float MaxSpeed = 3;
+		private const float Accel = 0.1f;
+		private const float Decel = 0.05f;
 
-		public Point Size = new Point(10, 15);
+		public Point Size = new Point(15, 10);
 		public float Angle = 0;
+		public float MoveSpeed = 0;
+		public float TrueX;
+		public float TrueY;
 
 		public Player() {
 			Collider = new NoCollider(this);
@@ -30,16 +35,34 @@ namespace JingleJam2024.entity {
 		}
 
 		public override void Update() {
-			if (Program.Input[GameControl.Left].Down) {
+			if (Program.Input[GameControl.Left].Down && MoveSpeed != 0) {
 				Angle -= TurnSpeed;
 			}
-			if (Program.Input[GameControl.Right].Down) {
+			if (Program.Input[GameControl.Right].Down && MoveSpeed != 0) {
 				Angle += TurnSpeed;
 			}
+			if (Program.Input[GameControl.Up].Down) {
+				MoveSpeed += Accel;
+				if (MoveSpeed > MaxSpeed) MoveSpeed = MaxSpeed;
+			} else if (Program.Input[GameControl.Down].Down) {
+				MoveSpeed -= Accel;
+				if (MoveSpeed < -MaxSpeed) MoveSpeed = -MaxSpeed;
+			} else {
+				if (MoveSpeed > 0) {
+					MoveSpeed -= Decel;
+					if (MoveSpeed < 0) MoveSpeed = 0;
+				} else if (MoveSpeed < 0) {
+					MoveSpeed += Decel;
+					if (MoveSpeed > 0) MoveSpeed = 0;
+				}
+			}
 
-			
-
-			base.Update();
+			var xspeed = MoveSpeed * (float)Math.Cos(Angle);
+			var yspeed = MoveSpeed * (float)Math.Sin(Angle);
+			TrueX += xspeed;
+			TrueY += yspeed;
+			X = (int)TrueX;
+			Y = (int)TrueY;
 		}
 
 	}
