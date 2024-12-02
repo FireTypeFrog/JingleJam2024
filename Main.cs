cdb29d91728631c460bfe7d341b90a581489d4d2
MattdaTheBest <121419969+MattdaTheBest@users.noreply.github.com>
2024-12-01 23:23:43 -0800

    Another test.

--- Main.cs
+++ Main.cs
@@ -10,14 +10,13 @@ using Toybox.utils.text;
 namespace JingleJam2024 {
 	public class Main:Core {
 
-// Hello this is my test comment, I hope you like it! :)
-
 		public Main() {
 			Program.Input.Add(GameControl.Left, new VirtualKey(Keys.A, Keys.Left));
 			Program.Input.Add(GameControl.Right, new VirtualKey(Keys.D, Keys.Right));
 			Program.Input.Add(GameControl.Up, new VirtualKey(Keys.W, Keys.Up));
 			Program.Input.Add(GameControl.Down, new VirtualKey(Keys.S, Keys.Down));
-		}
+            Program.Input.Add(GameControl.Down, new VirtualKey(Keys.Y, Keys.Down));
+        }
 
 		public override Scene GetActiveScene() {
 			return Program.Scene;
