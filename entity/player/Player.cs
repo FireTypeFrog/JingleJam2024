2e9da853418051f379777c83ae331125f1e73cfe
FireTypeFrog <firetypefrog@proton.me>
2024-12-03 22:21:27 -0800

    fixed player scale

--- entity/player/Player.cs
+++ entity/player/Player.cs
@@ -16,13 +16,14 @@ namespace JingleJam2024.entity.player {
 
 		public int X, Y;
 		public float TrueX, TrueY;
-		public Point Size = new Point(15, 10);
+		public Point Size = new Point(8, 5);
 		public Vector2 Speed;
 		public float Angle = 0;
 
 		public PlayerController Controller = new();
 		public PlayerCollider Collider = new();
 		public PresentTrail Trail = new();
+		public PresentShooter Shooter = new();
 
 		public Player() {
 		}
@@ -30,16 +31,19 @@ namespace JingleJam2024.entity.player {
 		public void Draw(Renderer r, Camera c) {
 			Trail.Draw(r, c);
 
-			var dest = new Rectangle(X, Y, Size.X, Size.Y);
+			var dest = new Rectangle(X, Y, Size.X * c.PixelScale, Size.Y * c.PixelScale);
 			var origin = new Vector2((float)r.Blank.Width / 2, (float)r.Blank.Height / 2);
 			dest = c.Project(Camera.Space.Pixel, Camera.Space.Render, dest);
 			r.Batch.Draw(r.Blank, dest, null, Color.White, Angle, origin, Microsoft.Xna.Framework.Graphics.SpriteEffects.None, 0);
+
+			Shooter.Draw(r, c);
 		}
 
 		public void Update() {
 			Controller.Update(this);
 			Collider.Move(this, Speed);
 			Trail.Update(this);
+			Shooter.Update(this);
 		}
 
 		public void DrawHitbox(Renderer r, Camera c) {
